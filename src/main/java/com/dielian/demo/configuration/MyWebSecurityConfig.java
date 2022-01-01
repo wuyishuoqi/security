d7f016e99e58d09ac964bb378cd06b9443ba1678
wuyishuoqi <1404493340@qq.com>
2022-01-02 01:18:43 +0800

    dynamic url resources

--- src/main/java/com/dielian/demo/configuration/MyWebSecurityConfig.java
+++ src/main/java/com/dielian/demo/configuration/MyWebSecurityConfig.java
@@ -4,6 +4,8 @@ import com.dielian.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -23,6 +25,13 @@ public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth)throws Exception{
                  auth.userDetailsService(userService);
+    }
+    @Bean
+    RoleHierarchy roleHierarchy(){
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        String hierarchy="ROLE_dba > ROLE_admin > ROLE_user";
+        roleHierarchy.setHierarchy(hierarchy);
+        return roleHierarchy;
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
